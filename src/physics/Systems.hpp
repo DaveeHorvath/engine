87c63b4007ba9d07d062c49c71afeb7feaf6d0d4
dhorvath <dhorvath@student.hive.fi>
2024-10-02 01:50:39 +0300

    removed as its unused and not useful atm

--- src/physics/Systems.hpp
+++ src/physics/Systems.hpp
@@ -1,15 +1,5 @@
 #ifndef PHYSICS_SYSTEM
 #define PHYSICS_SYSTEM
-#include "Components.hpp"
-#include "Constraint.hpp"
 
-#include "Register.hpp"
-
-class PhysicsSystem {
-    Registry<Transform, Rigidbody> physicsRegister;
-    inline static PhysicsSystem* instance;
-    std::vector<Constraint> constraints;
-    PhysicsSystem();
-};
 
 #endif
