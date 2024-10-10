c871258112c3bb1c0d191b5a555b04af34d646af
dhorvath <dhorvath@student.hive.fi>
2024-10-10 13:42:27 +0300

    correct colors

--- src/render/Swapchain.cpp
+++ src/render/Swapchain.cpp
@@ -143,7 +143,7 @@ VkSurfaceFormatKHR pickSurfaceFormat(const std::vector<VkSurfaceFormatKHR> forma
 {
     for (const auto& format : formats) 
     {
-        if (format.format == VK_FORMAT_B8G8R8A8_SRGB && format.colorSpace == VK_COLOR_SPACE_SRGB_NONLINEAR_KHR)
+        if (format.format == VK_FORMAT_R8G8B8A8_UNORM && format.colorSpace == VK_COLOR_SPACE_SRGB_NONLINEAR_KHR)
             return format;
     }
     return formats[0];
